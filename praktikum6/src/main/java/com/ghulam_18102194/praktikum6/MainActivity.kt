ee4f27fdadf3bf02d8ed1acda00c9eae5964ffb8
Ghulam Ammar Yanuar <ghulam.yanuar01@gmail.com>
2020-11-15 10:00:05 +0700

    init prak 6

---
+++ praktikum6/src/main/java/com/ghulam_18102194/praktikum6/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ghulam_18102194.praktikum6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
