4066de695ee0b2bec85bfe74d3f256e027d1c1b4
novian66 <novian66@gmail.com>
2020-11-19 11:12:32 +0700

    create new activity for testing

---
+++ app/src/main/java/com/ghulam_18102194/lifecycleapp/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.ghulam_18102194.lifecycleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
