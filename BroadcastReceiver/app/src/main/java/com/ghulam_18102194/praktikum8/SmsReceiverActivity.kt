118c5cabe10462e13d9e34716bdfefbbcbf9ccbf
Ghulam Ammar Yanuar <ghulam.yanuar01@gmail.com>
2020-12-17 15:57:39 +0700

    Add Toast

--- BroadcastReceiver/app/src/main/java/com/ghulam_18102194/praktikum8/SmsReceiverActivity.kt
+++ BroadcastReceiver/app/src/main/java/com/ghulam_18102194/praktikum8/SmsReceiverActivity.kt
@@ -2,6 +2,7 @@ package com.ghulam_18102194.praktikum8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_sms_receiver.*
 
@@ -14,13 +15,26 @@ class SmsReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sms_receiver)
-
-        title = getString(R.string.incoming_massage)
-        val senderNo = intent.getStringExtra(EXTRA_SMS_NO)
-        val senderMessage = intent.getStringExtra(EXTRA_SMS_MESSAGE)
+        title = getString(R.string.incoming_message)
+        val senderNo = intent.getStringExtra(com.ghulam_18102194.praktikum8.SmsReceiverActivity.EXTRA_SMS_NO)
+        val senderMessage = intent.getStringExtra(com.ghulam_18102194.praktikum8.SmsReceiverActivity.EXTRA_SMS_MESSAGE)
         val match = keywords.filter { it in senderMessage!!.split(" ") }
         if(match.isNotEmpty()){
             activity_sms_receiver.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_red_dark))
+        }
+
+        senderMessage?.let {
+            var wordcount = 0;
+            keywords.map { word ->
+                if (it.contains(word, true)){
+                    wordcount++
+                }
+            }
+            println("forbiddenWordCount $wordcount")
+
+            if (wordcount >= 1){
+                Toast.makeText(this, "Hati_hati!!! Sms Ini Penipuan!!!", Toast.LENGTH_SHORT).show()
+            }
         }
         tv_from.text = getString(R.string.coming_from)+": "+senderNo
         tv_message.text = senderMessage
