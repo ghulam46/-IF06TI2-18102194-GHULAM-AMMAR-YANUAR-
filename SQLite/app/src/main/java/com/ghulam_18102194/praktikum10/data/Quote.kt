94b6c648ab4d1bcff9412474f4d5a76def17f304
Ghulam Ammar Yanuar <ghulam.yanuar01@gmail.com>
2021-01-06 20:33:31 +0700

    add QuoteAdapter.kt, Quote.kt, MainActivity.kt ,file xlm

--- SQLite/app/src/main/java/com/ghulam_18102194/praktikum10/data/Quote.kt
+++ SQLite/app/src/main/java/com/ghulam_18102194/praktikum10/data/Quote.kt
@@ -3,7 +3,6 @@ package com.ghulam_18102194.praktikum10.data
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-class Quote(id: Int, title: String, description: String, category: String, date: String) {
     @Parcelize
     data class Quote(
         var id: Int = 0,
@@ -12,5 +11,3 @@ class Quote(id: Int, title: String, description: String, category: String, date:
         var category: String? = null,
         var date: String? = null
     ) : Parcelable
-
-}
