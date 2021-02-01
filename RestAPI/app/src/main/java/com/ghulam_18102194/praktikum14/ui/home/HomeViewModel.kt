7051728aaaa52574c0f4ba6c2441456db04b4bdc
Ghulam Ammar Yanuar <ghulam.yanuar01@gmail.com>
2021-02-01 09:45:45 +0700

    init praktikum 14

---
+++ RestAPI/app/src/main/java/com/ghulam_18102194/praktikum14/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.ghulam_18102194.praktikum14.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
