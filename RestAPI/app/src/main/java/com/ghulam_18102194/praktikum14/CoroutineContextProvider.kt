8d285d0119159e5311a61092329dcef9a80b8a3c
Ghulam Ammar Yanuar <ghulam.yanuar01@gmail.com>
2021-02-02 14:56:56 +0700

    add CoroutineContextProvider.kt

---
+++ RestAPI/app/src/main/java/com/ghulam_18102194/praktikum14/CoroutineContextProvider.kt
@@ -0,0 +1,8 @@
+package com.ghulam_18102194.praktikum14
+
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+open class CoroutineContextProvider {
+    open val main: CoroutineContext by lazy { Dispatchers.Main }
+}
